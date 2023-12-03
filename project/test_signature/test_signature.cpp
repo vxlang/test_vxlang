371afee12001a04898be2ea6914c34be528486d9
0a777h <0a777h@gmail.com>
2023-12-03 20:21:09 +0900

    signature-test, clang

--- project/test_signature/test_signature.cpp
+++ project/test_signature/test_signature.cpp
@@ -1,7 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#include "vxlib_signature.h"
+#define CODE_FLA_BEGIN \
+__asm__(\
+   "sti \n"\
+   "sti \n"\
+   "sti \n"\
+   "nop \n"\
+   "hlt \n"\
+)
+
+#define CODE_FLA_END \
+__asm__(\
+   "cli \n"\
+   "cli \n"\
+   "cli \n"\
+   "nop \n"\
+   "hlt \n"\
+);
+
+//
 
 int main() {
 
