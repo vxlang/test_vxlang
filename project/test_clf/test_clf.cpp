1c65cb235fe8b9ff162f1507d0bd442478d6f474
0a777h <0a777h@gmail.com>
2024-02-16 22:54:27 +0900

    update

--- project/test_clf/test_clf.cpp
+++ project/test_clf/test_clf.cpp
@@ -19,13 +19,75 @@ void lv1() {
     return;
 }
 
+void seh() {
+    VL_CODE_FLATTENING_BEGIN;
+
+    __try {
+        //printf("CodeFlattening \n");
+        __debugbreak();
+    }
+    __except (1) {
+        printf("__except ! \n");
+    }
+
+    VL_CODE_FLATTENING_END;
+
+
+    //
+
+    __try {
+        __debugbreak();
+        __debugbreak();
+    }
+    __except (1) {
+        printf("ObfuscateSEH_1:: SEH 2 \n");
+    }
+
+    //
+
+    VL_CODE_FLATTENING_BEGIN;
+
+    __try {
+        __debugbreak();
+    }
+    __except (1) {
+        printf("ObfuscateSEH_1:: SEH 3 \n");
+    }
+
+    VL_CODE_FLATTENING_END;
+
+    return;
+}
+
 int main() {
     VL_CODE_FLATTENING_BEGIN;
 
-    int i = 0;
-    while (true) {
-        lv1();
-        Sleep(1000);
+    seh();
+
+    VL_CODE_FLATTENING_END;
+
+    //
+
+    VL_CODE_FLATTENING_BEGIN;
+
+    for (int i = 0; i < 10; ++i) {
+        printf("%d \n", i);
+        Sleep(100);
+    }
+
+    VL_CODE_FLATTENING_END;
+
+    //
+
+    int i = 10;
+
+    VL_CODE_FLATTENING_BEGIN;
+
+    if (10 == i) {
+        printf(" :] ! \n");
+    }
+    else {
+        printf(" :[ ! \n");
     }
 
     VL_CODE_FLATTENING_END;
