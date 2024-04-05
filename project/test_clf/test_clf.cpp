91804d60a39b13be0163cca04c9e26d78c99ff5b
0a777h <0a777h@gmail.com>
2024-04-06 08:01:39 +0900

    update

--- project/test_clf/test_clf.cpp
+++ project/test_clf/test_clf.cpp
@@ -9,62 +9,12 @@
 
 //
 
-void lv1() {
-    VL_CODE_FLATTENING_LV_BEGIN(1);
-
-    printf("LV 1 \n");
-
-    VL_CODE_FLATTENING_LV_END(1);
-
-    return;
-}
-
 void hello() {
     VL_CODE_FLATTENING_BEGIN;
 
     for (int i = 0; i < 10; ++i) {
         printf("Hello, World! \n");
         Sleep(100);
-    }
-
-    VL_CODE_FLATTENING_END;
-
-    return;
-}
-
-void seh() {
-    VL_CODE_FLATTENING_BEGIN;
-
-    __try {
-        //printf("CodeFlattening \n");
-        __debugbreak();
-    }
-    __except (1) {
-        printf("__except ! \n");
-    }
-
-    VL_CODE_FLATTENING_END;
-
-
-    //
-
-    __try {
-        __debugbreak();
-        __debugbreak();
-    }
-    __except (1) {
-        printf("ObfuscateSEH_1:: SEH 2 \n");
-    }
-
-    //
-
-    VL_CODE_FLATTENING_BEGIN;
-
-    __try {
-        __debugbreak();
-    }
-    __except (1) {
-        printf("ObfuscateSEH_1:: SEH 3 \n");
     }
 
     VL_CODE_FLATTENING_END;
@@ -79,35 +29,6 @@ int main() {
 
     VL_CODE_FLATTENING_END;
 
-    //
-
-#if 0
-    VL_CODE_FLATTENING_BEGIN;
-
-    seh();
-
-    for (int i = 0; i < 10; ++i) {
-        printf("%d \n", i);
-        Sleep(100);
-    }
-
-    VL_CODE_FLATTENING_END;
-
-    //
-
-    int i = 10;
-
-    VL_CODE_FLATTENING_BEGIN;
-
-    if (10 == i) {
-        printf(" :] ! \n");
-    }
-    else {
-        printf(" :[ ! \n");
-    }
-
-    VL_CODE_FLATTENING_END;
-#endif
     return 1;
 }
 
